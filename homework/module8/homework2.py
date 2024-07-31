ea987d1f9c68d3f37cd6687972032ec658594fb5
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-07-31 13:49:55 +0400

    .

--- homework/module8/homework2.py
+++ homework/module8/homework2.py
@@ -1,16 +1,13 @@
 def add_everything_up(a, b):
-    # Проверка на разные типы
     if (isinstance(a, (int, float)) and isinstance(b, str)) or (isinstance(a, str) and isinstance(b, (int, float))):
         return str(a) + str(b)
 
-    # Если оба числа или обе строки
     try:
         return a + b
     except TypeError:
         return str(a) + str(b)
 
 
-# Примеры использования
-print(add_everything_up(123.456, 'строка'))  # 123.456строка
-print(add_everything_up('яблоко', 4215))  # яблоко4215
-print(add_everything_up(123.456, 7))  # 130.456
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
