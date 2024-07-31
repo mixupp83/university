42d1b451e0a772a7a3ba9da1a07fc58cd6de5152
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-07-31 13:53:35 +0400

    .

---
+++ homework/module8/homework3.py
@@ -0,0 +1,29 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for number in numbers:
+        try:
+            result += number
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {number}')
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    try:
+        if not isinstance(numbers, (list, tuple, set)):
+            raise TypeError
+        total_sum, incorrect_data = personal_sum(numbers)
+        if len(numbers) - incorrect_data == 0:
+            raise ZeroDivisionError
+        return total_sum / (len(numbers) - incorrect_data)
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
