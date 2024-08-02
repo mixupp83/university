d11f74ac9d3416d187ef43128c07ab6fe4be96bb
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-08-02 13:49:53 +0400

    .

---
+++ homework/module10/homework1.py
@@ -0,0 +1,33 @@
+import time
+import threading
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {i}\n")
+            time.sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+start_time_functions = time.time()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+end_time_functions = time.time()
+print(f"Работа функций {end_time_functions - start_time_functions}")
+
+start_time_threads = time.time()
+
+threads = []
+for word_count, file_name in [(10, 'example5.txt'), (30, 'example6.txt'), (200, 'example7.txt'), (100, 'example8.txt')]:
+    thread = threading.Thread(target=write_words, args=(word_count, file_name))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end_time_threads = time.time()
+print(f"Работа потоков {end_time_threads - start_time_threads}")
