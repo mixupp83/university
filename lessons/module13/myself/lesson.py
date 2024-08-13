774db9a7883936ea50fcba7fa29e6e4f5662f853
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-08-13 12:58:21 +0400

    .

--- lessons/module13/myself/lesson.py
+++ lessons/module13/myself/lesson.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7249341772:AAHHFBW3zLzr2Aco1eN1fu9Y-C4hJ_1ZaWY"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
