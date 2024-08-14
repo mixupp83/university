c1f1e3e637b25ff0f23c3904184f122b94a386d5
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-08-14 11:27:17 +0400

    .

--- lessons/module13/myself/lesson.py
+++ lessons/module13/myself/lesson.py
@@ -6,5 +6,21 @@ api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
+@dp.message_handler(text=["Urban"])
+async def urban_massege(message):
+    print('Urban message')
+
+
+@dp.message_handler(commands=['start'])
+async def start_messadge(message):
+    print('Start message')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Мы получили сообщение')
+
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
