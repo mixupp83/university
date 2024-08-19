28a8e60197d6261c102a120331565680ab4f376e
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-08-19 10:51:32 +0400

    .

--- lessons/module13/myself/lesson.py
+++ lessons/module13/myself/lesson.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = ""
+api = "7249341772:AAHHFBW3zLzr2Aco1eN1fu9Y-C4hJ_1ZaWY"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -10,16 +10,19 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(text=["Urban"])
 async def urban_massege(message):
     print('Urban message')
+    await message.answer('Urban message')
 
 
 @dp.message_handler(commands=['start'])
 async def start_messadge(message):
     print('Start message')
+    await message.answer('Рады Вас видеть в нашем боте!')
 
 
 @dp.message_handler()
 async def all_message(message):
     print('Мы получили сообщение')
+    await message.answer(message.text)
 
 
 if __name__ == "__main__":
